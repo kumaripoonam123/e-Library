27960457ead212bff17b2971c7e387bffc3e9994
Poonam Kumari <96419234+kumaripoonam123@users.noreply.github.com>
2023-09-22 22:14:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,118 @@
+console.log("This is index.js");
+//Todos"
+/*
+1.Store all the data in local Storage
+2.Give another columnas an aption to delete the book
+3.Add a scroll bar to the view
+*/
+
+
+
+
+//Constuctor
+function Book(name, author, type) {
+    this.name = name;
+    this.author = author;
+    this.type = type;
+}
+
+//Display Constructor
+function Display() {
+
+}
+//Add methods to display prototype
+Display.prototype.add = function(book) {
+    console.log("Adding to UI");
+    tableBody = document.getElementById('tableBody');
+    let uiString = `
+    
+           <tr> 
+                       
+            <td>${book.name}</td>
+            <td>${book.author}</td>
+            <td>${book.type}</td>
+            </tr>`;
+            tableBody.innerHTML+=uiString;
+           
+}
+//Implement the clear function
+Display.prototype.clear = function () {
+    let libraryForm = document.getElementById('libraryForm');
+    libraryForm.reset();
+}
+//Implement the validate function
+Display.prototype.validate = function (book) {
+    if(book.name.length<2||book.name.length<2||book.name.length<2)
+    {
+        return false
+    }
+    else{
+        return true;
+}
+}
+Display.prototype.show = function(type,displayMessage) {
+    let message =document.getElementById('message');
+    let boldText;
+    if(type==="success"){
+        boldText='Success!';
+}
+    else{
+boldText='Error!';
+    }
+    message.innerHTML=`<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+    <strong>${boldText}</strong> ${displayMessage} 
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>`;
+}
+setTimeout(()=>{
+    message.innerHTML=''},5000);
+
+
+//Add submit event listener
+let libraryForm = document.getElementById('libraryForm');
+libraryForm.addEventListener('submit', libraryFormSubmit);
+
+function libraryFormSubmit(e) {
+    console.log("You have submitted library form");
+    let name = document.getElementById('bookName').value;
+    let author = document.getElementById('author').value;
+    //fiction,programming,cooking
+
+
+    let fiction = document.getElementById('fiction');
+    let programming = document.getElementById('programming');
+    let cooking = document.getElementById('cooking');
+    let type;
+
+    if (fiction.checked) {
+        type = fiction.value;
+
+    }
+    else if (programming.checked) {
+        type = programming.value;
+    }
+    else if (cooking.checked) {
+        type = cooking.value;
+    }
+    
+    
+    
+    let book = new Book(name, author, type);
+    console.log(book);
+    
+    let display =new Display();
+    if(display.validate(book)){
+        display.add(book);
+        display.clear();
+        display.show('success','Your book has been successfully added');
+    }
+    else{
+        //Show error to the user
+        display.show('danger','Sorry you cannot add this book');
+    }
+    
+    
+   
+    e.preventDefault();
+
+}
